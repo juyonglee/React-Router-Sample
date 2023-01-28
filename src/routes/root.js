3ca996c86cda9db61e87d1d6439e89008beaf577
juyonglee <juyonglee0208@gmail.com>
2023-01-28 09:06:42 +0900

    Active Link Styling 구현

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,5 +1,12 @@
-import {Link, Outlet, useLoaderData, Form, redirect} from "react-router-dom";
-import {getContacts, createContact} from "../contacts";
+import {
+  Form,
+  Link,
+  NavLink,
+  Outlet,
+  redirect,
+  useLoaderData
+} from "react-router-dom";
+import {createContact, getContacts} from "../contacts";
 
 export async function loader() {
   const contacts = await getContacts();
@@ -45,16 +52,27 @@ export default function Root() {
                 <ul>
                   {contacts.map((contact) => (
                       <li key={contact.id}>
-                        <Link to={`contacts/${contact.id}`}>
-                          {contact.first || contact.last ? (
-                              <>
-                                {contact.first} {contact.last}
-                              </>
-                          ) : (
-                              <i>No Name</i>
-                          )}{" "}
-                          {contact.favorite && <span>★</span>}
-                        </Link>
+                        <NavLink
+                            to={`contacts/${contact.id}`}
+                            className={({ isActive, isPending }) =>
+                                isActive
+                                    ? "active"
+                                    : isPending
+                                        ? "pending"
+                                        : ""
+                            }
+                        >
+                          <Link to={`contacts/${contact.id}`}>
+                            {contact.first || contact.last ? (
+                                <>
+                                  {contact.first} {contact.last}
+                                </>
+                            ) : (
+                                <i>No Name</i>
+                            )}{" "}
+                            {contact.favorite && <span>★</span>}
+                          </Link>
+                        </NavLink>
                       </li>
                   ))}
                 </ul>
