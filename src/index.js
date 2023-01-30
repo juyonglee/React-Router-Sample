a360994bb2c74b3fb43255629a687c6dd224dd80
juyonglee <juyonglee0208@gmail.com>
2023-01-31 01:57:38 +0900

    index router 설정 추가

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import Contact, {loader as contactLoader} from "./routes/contact";
 import Root, {action as rootAction, loader as rootLoader} from "./routes/root";
 import EditContact, {action as editAction} from "./routes/edit";
 import {action as destroyAction} from "./routes/destroy";
+import Index from "./routes/index";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+      {
+        index: true,
+        element: <Index/>
+      },
       {
         path: "contacts/:contactId",
         element: <Contact/>,
