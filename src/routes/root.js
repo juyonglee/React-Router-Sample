799ebe0046df33df74b403e2bf464f921feeff8f
juyonglee <juyonglee0208@gmail.com>
2023-01-31 03:43:51 +0900

    Search Spinner 추가

--- src/routes/root.js
+++ src/routes/root.js
@@ -5,15 +5,17 @@ import {
   Outlet,
   redirect,
   useLoaderData,
-  useNavigation
+  useNavigation,
+  useSubmit
 } from "react-router-dom";
 import {createContact, getContacts} from "../contacts";
+import {useEffect} from "react";
 
 export async function loader({request}) {
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
   const contacts = await getContacts(q);
-  return {contacts};
+  return {contacts, q};
 }
 
 export async function action() {
@@ -22,8 +24,20 @@ export async function action() {
 }
 
 export default function Root() {
-  const {contacts} = useLoaderData();
+  const {contacts, q} = useLoaderData();
   const navigation = useNavigation();
+  const submit = useSubmit();
+
+  const searching =
+      navigation.location &&
+      new URLSearchParams(navigation.location.search).has(
+          "q"
+      );
+
+  useEffect(() => {
+    document.getElementById("q").value = q;
+  }, [q]);
+
   return (
       <>
         <div id="sidebar">
@@ -32,15 +46,20 @@ export default function Root() {
             <Form id="search-form" role="search">
               <input
                   id="q"
+                  className={searching ? "loading" : ""}
                   aria-label="Search contacts"
                   placeholder="Search"
                   type="search"
                   name="q"
+                  defaultValue={q}
+                  onChange={(event) => {
+                    submit(event.currentTarget.form);
+                  }}
               />
               <div
                   id="search-spinner"
                   aria-hidden
-                  hidden={true}
+                  hidden={!searching}
               />
               <div
                   className="sr-only"
